910c09680749d6dd28175bd1916dad64768d8238
Matthieu Dorier <mdorier@anl.gov>
2022-06-21 12:32:23 +0000

    added more logging

--- src/colza-ascent-pipeline.cpp
+++ src/colza-ascent-pipeline.cpp
@@ -1,4 +1,5 @@
 #include <colza/Backend.hpp>
+#include <spdlog/spdlog.h>
 #include <thallium.hpp>
 #include <mona.h>
 #include <mona-mpi.h>
@@ -25,11 +26,26 @@ class AscentPipeline : public colza::Backend {
             auto options = args.config["ascent_options"].dump();
             m_ascent_options.parse(options);
         }
+        if(args.config.contains("comm_type")) {
+            if(!args.config["comm_type"].is_string()) {
+                throw std::runtime_error("AscentPipeline configuration: comm_type should be a string");
+            }
+            auto comm_type = args.config["comm_type"].get<std::string>();
+            if(comm_type == "mpi") {
+                m_comm_type = CommType::MPI;
+            } else if(comm_type == "mona") {
+                m_comm_type = CommType::MONA;
+            } else {
+                throw std::runtime_error("AscentPipeline configuration: comm_type should be \"mona\" or \"mpi\"");
+            }
+        }
     }
 
     void updateMonaAddresses(mona_instance_t mona, const std::vector<na_addr_t>& addresses) override {
         if(m_comm_type == CommType::MPI) {
             m_mpi_comm = MPI_COMM_WORLD;
+            spdlog::trace("AscentPipeline using MPI_COMM_WORLD");
+            m_ascent_options["mpi_comm"] = MPI_COMM_WORLD;
             return;
         }
         if(m_mpi_comm != MPI_COMM_NULL) {
@@ -49,23 +65,27 @@ class AscentPipeline : public colza::Backend {
             throw std::runtime_error("MPI_Register_mona_comm failed");
         }
         m_ascent_options["mpi_comm"] = MPI_Comm_c2f(m_mpi_comm);
+        spdlog::trace("AscentPipeline::updateMonaAddresses succeeded");
     }
 
     colza::RequestResult<int32_t> start(uint64_t iteration) override {
         auto result      = colza::RequestResult<int32_t>{};
         result.value()   = 0;
         result.success() = true;
+        spdlog::trace("AscentPipeline::start() called with iteration {}", iteration);
         return result;
     }
 
     void abort(uint64_t iteration) override {
         (void)iteration;
+        spdlog::trace("AscentPipeline::abort() called with iteration {}", iteration);
     }
 
     colza::RequestResult<int32_t> stage(const std::string& sender_addr,
             const std::string& dataset_name, uint64_t iteration, uint64_t block_id,
             const std::vector<size_t>& dimensions, const std::vector<int64_t>& offsets,
             const colza::Type& type, const thallium::bulk& remote_data) override {
+        spdlog::trace("AscentPipeline::stage() called with iteration {}", iteration);
         auto result = colza::RequestResult<int32_t>{};
         (void)offsets;
         (void)type;
@@ -87,22 +107,26 @@ class AscentPipeline : public colza::Backend {
         // Store data
         std::lock_guard<tl::mutex> g(m_data_mtx);
         m_data[iteration][dataset_name].update(std::move(data));
+        spdlog::trace("AscentPipeline::stage() completed iteration {}", iteration);
         return result;
     }
 
     colza::RequestResult<int32_t> execute(uint64_t iteration) override {
         auto result = colza::RequestResult<int32_t>{};
+        spdlog::trace("AscentPipeline::execute() called with iteration {}", iteration);
         ascent::Ascent ascent;
         ascent.open(m_ascent_options);
         ascent.publish(m_data[iteration]["mesh"]);
         conduit::Node a; // actions are actually defined in options (via actions_file)
         ascent.execute(a);
         ascent.close();
+        spdlog::trace("AscentPipeline::execute() completed iteration {}", iteration);
         return result;
     }
 
     colza::RequestResult<int32_t> cleanup(uint64_t iteration) override {
         auto result = colza::RequestResult<int32_t>{};
+        spdlog::trace("AscentPipeline::cleanup() called iteration {}", iteration);
         std::lock_guard<tl::mutex> g(m_data_mtx);
         m_data.erase(iteration);
         result.value() = 0;
@@ -111,6 +135,7 @@ class AscentPipeline : public colza::Backend {
 
     colza::RequestResult<int32_t> destroy() override {
         auto result = colza::RequestResult<int32_t>{};
+        spdlog::trace("AscentPipeline::destroy() called iteration");
         result.value() = 0;
         result.success() = true;
         return result;
